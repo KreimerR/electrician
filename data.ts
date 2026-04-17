a82bd947230a25efc45953eb5426a0ce1dabc454
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-17 11:37:26 +0300

    feat: add data

--- data.ts
+++ data.ts
@@ -1,17 +1,83 @@
 export const data = {
   en: {
+    logo: "Hashmalay",
+    nav: [
+      {
+        id: "#home",
+        title: "Home",
+      },
+      {
+        id: "#about",
+        title: "About",
+      },
+      {
+        id: "#services",
+        title: "Services",
+      },
+      {
+        id: "#contact",
+        title: "Contact",
+      },
+    ],
+    button: "Contact me",
     hero: {
-      h1: "Hello!",
+      h1: "Powering Your Home with Safety & Precision",
+      h2: "Licensed • Insured • Fast Response in the Center",
+      button: "Contact me",
     },
   },
   he: {
+    logo: "חשמלאי",
+    nav: [
+      {
+        id: "#home",
+        title: "בית",
+      },
+      {
+        id: "#about",
+        title: "אודות",
+      },
+      {
+        id: "#services",
+        title: "שירותים",
+      },
+      {
+        id: "#contact",
+        title: "תתקשר אליי",
+      },
+    ],
+    button: "תתקשר אליי",
     hero: {
-      h1: "שלום!",
+      h1: "מחשמלים את הבית שלכם בבטיחות ובדיוק",
+      h2: "מורשה • מבוטח • מענה מהיר במרכז הארץ",
+      button: "תתקשר אליי",
     },
   },
   ru: {
+    logo: "Хашмалай",
+    nav: [
+      {
+        id: "#home",
+        title: "Домой",
+      },
+      {
+        id: "#about",
+        title: "Обо мне",
+      },
+      {
+        id: "#services",
+        title: "Услуги",
+      },
+      {
+        id: "#contact",
+        title: "Связаться",
+      },
+    ],
+    button: "Свяжись со мной",
     hero: {
-      h1: "Привет!",
+      h1: "Электроснабжая Ваш Дом с Безопасностью и Точностью",
+      h2: "Лицензированный • Застрахованный • Быстрый выезд по центру",
+      button: "Свяжись со мной",
     },
   },
 }
