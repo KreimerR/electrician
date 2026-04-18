afe4bac59c265b310ea1233021021fd09ef26bbe
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-18 18:51:37 +0300

    feat: update the Info component and add the SecondInfo component

--- components/info/FirstInfo.tsx
+++ components/info/FirstInfo.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import { useDataContext } from "./DataProvider"
+import { useDataContext } from "../DataProvider"
 
-export default function Info() {
+export default function FirstInfo() {
   const data = useDataContext()
 
   return (
     <section className="h-[100px] bg-brand-black px-20 flex justify-between items-center gap-5">
-      {data.content.info.map((el: string, index: number) =>
+      {data.content.firstInfo.map((el: string, index: number) =>
         <h1 key={index} className="text-2xl text-brand-white font-bold">{el}</h1>
       )}
     </section>
---
+++ components/info/SecondInfo.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useDataContext } from "../DataProvider"
+
+export default function SecondInfo() {
+  const data = useDataContext()
+
+  return (
+    <section className="h-[100px] bg-brand-black px-20 flex justify-between items-center gap-5">
+      {data.content.secondInfo.map((el: string, index: number) =>
+        <h1 key={index} className="text-2xl text-brand-white font-bold">{el}</h1>
+      )}
+    </section>
+  )
+}
