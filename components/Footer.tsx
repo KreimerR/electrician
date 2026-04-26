2a49bcfb1159cfbefc127faf959fdd6c3954cb5f
Rodion Kreimer <kreimerrodion@gmail.com>
2026-04-26 19:25:52 +0300

    feat: add the Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
-export default function Footer() {
-  return (
-    <footer>
+"use client"
 
+import { useDataContext } from "./DataProvider"
+
+export default function Footer() {
+  const data = useDataContext()
+
+  return (
+    <footer className="bg-gray-950 w-full p-10 text-lg text-white text-center font-[600]">
+      © {new Date().getFullYear()} {data.content.footer}
     </footer>
   )
 }
