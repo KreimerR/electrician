46e123ea0ccc7798ec2dfd0fca3530bbf2a06843
Rodion Kreimer <kreimerrodion@gmail.com>
2026-05-15 10:52:44 +0300

    update: update ServiceCards component

--- components/services/ServiceCards.tsx
+++ components/services/ServiceCards.tsx
@@ -8,10 +8,10 @@ export default function ServiceCards() {
   const data = useDataContext()
 
   return (
-    <div className="flex flex-col gap-5 2xl:grid 2xl:grid-cols-[1fr_1fr_1fr_1fr] 2xl:gap-7">
+    <div className="flex flex-col gap-5 lg:flex-row lg:flex-wrap lg:justify-center">
       {data.content.services.serviceCards.map((el: { icon: StaticImageData, title: string }, index: number) =>
-        <ServiceCard key={index} icon={el.icon} title={el.title} lang={data.lang} />
+        <ServiceCard key={index} icon={el.icon} title={el.title} />
       )}
     </div>
   )
-} 
+}
