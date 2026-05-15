a4b8fb5d79f11b5daec969f492762a06fbc8b708
Rodion Kreimer <kreimerrodion@gmail.com>
2026-05-15 10:31:34 +0300

    style: add drop-shadow style to the ContactButton component

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -9,7 +9,7 @@ export default function ContactButton() {
   return (
     <button
       onClick={() => scrollToSection("contact")}
-      className="py-2 px-4 bg-brand-white text-lg text-center text-brand-black font-[600] rounded-2xl transition-all hover:cursor-pointer hover:bg-brand-white-darker"
+      className="py-2 px-4 bg-brand-white text-lg text-center text-brand-black font-[600] rounded-2xl transition-all hover:cursor-pointer hover:bg-brand-white-darker drop-shadow-sm"
     >
       {data.content.button}
     </button>
