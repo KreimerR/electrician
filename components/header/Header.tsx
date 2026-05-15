c620099980650f2a2cca24267b581d16e934a5ad
Rodion Kreimer <kreimerrodion@gmail.com>
2026-05-15 10:55:10 +0300

    update: update the Header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,15 +1,24 @@
 "use client"
 
+import { scrollToSection } from "@/helper";
 import { useDataContext } from "../DataProvider";
-import HebrewHeader from "./HebrewHeader";
-import OtherHeaders from "./OtherHeaders";
+import Nav from "./Nav";
+import HeaderButtons from "./HeaderButtons";
 
 export default function Header() {
   const data = useDataContext()
 
   return (
     <header className="flex justify-between items-center w-screen fixed top-0 left-0 p-5 z-10">
-      {data.lang === "he" ? <HebrewHeader /> : <OtherHeaders />}
+      <button
+        onClick={() => scrollToSection("home")}
+        className="text-2xl text-brand-white font-bold transition-all hover:cursor-pointer hover:text-brand-white-darker drop-shadow-sm"
+      >
+        {data.content.logo}
+      </button>
+
+      <Nav modal={false} setIsMenuOpen={false} />
+      <HeaderButtons />
     </header>
   )
 }
