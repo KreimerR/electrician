c0a36ce7f9020a33019ca424037ddf7ad265f536
Rodion Kreimer <kreimerrodion@gmail.com>
2026-05-15 10:30:44 +0300

    fix: update import for Hero in page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,6 +1,6 @@
 import About from "@/components/about/About";
 import Contact from "@/components/contact/Contact";
-import Hero from "@/components/Hero";
+import Hero from "@/components/hero/Hero";
 import Info from "@/components/Info";
 import Services from "@/components/services/Services";
 
