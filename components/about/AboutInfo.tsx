8c02f8f47a28ac905be228242e5a7e8bcbd683eb
Rodion Kreimer <kreimerrodion@gmail.com>
2026-05-15 10:51:19 +0300

    update: update the AboutInfo component

--- components/about/AboutInfo.tsx
+++ components/about/AboutInfo.tsx
@@ -6,10 +6,10 @@ export default function AboutInfo() {
   const data = useDataContext()
 
   return (
-    <div className={`flex flex-col justify-center gap-4 h-screen lg:pr-20 px-5 lg:px-0 ${data.lang === "he" && "text-right"} z-5`}>
-      <h1 className="text-5xl text-brand-white font-bold">{data.content.about.h1}</h1>
-      <p className="text-lg text-brand-white font-semibold">{data.content.about.p}</p>
-      <h3 className="text-2xl text-brand-white font-bold">{data.content.about.h3}</h3>
+    <div className="flex flex-col justify-center gap-4 h-screen lg:pr-20 px-5 lg:px-0 z-5 text-brand-white drop-shadow-lg">
+      <h1 className="text-5xl font-bold">{data.content.about.h1}</h1>
+      <p className="text-lg font-semibold">{data.content.about.p}</p>
+      <h3 className="text-2xl font-bold">{data.content.about.h3}</h3>
     </div>
   )
 }
