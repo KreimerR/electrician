0541b293deadac63ae9573991b38b5e306a02609
Rodion Kreimer <kreimerrodion@gmail.com>
2026-05-15 10:51:06 +0300

    update: update the AboutImages component

--- components/about/AboutImages.tsx
+++ components/about/AboutImages.tsx
@@ -1,45 +1,33 @@
-"use client"
-
 import Image from "next/image";
-import aboutElectricianOtherSide from "@/public/about/electrician-about-other-side.png"
-import aboutBackgroundOtherSide from "@/public/about/electrician-about-background-other-side.png"
-import gradient from "@/public/assets/gradient.png"
 import aboutElectrician from "@/public/about/electrician-about.png"
 import aboutBackground from "@/public/about/electrician-about-background.png"
 import gradientOtherSide from "@/public/assets/gradient-other-side.png"
-import { useDataContext } from "../DataProvider";
 
 export default function AboutImages() {
-  const data = useDataContext()
-
   return (
     <>
       <Image
-        src={data.lang === "he" ? aboutElectricianOtherSide : aboutElectrician}
+        src={aboutElectrician}
         alt="Electrician"
-        width={data.lang === "he" ? aboutElectricianOtherSide.width : aboutElectrician.width}
-        height={data.lang === "he" ? aboutElectricianOtherSide.height : aboutElectrician.height}
-        className="object-contain h-screen pt-15 hidden lg:block"
+        width={aboutElectrician.width}
+        height={aboutElectrician.height}
+        className="object-contain h-screen pt-15 hidden lg:block rtl:scale-x-[-1]"
       />
 
       <Image
-        src={data.lang === "he" ? gradient : gradientOtherSide}
+        src={gradientOtherSide}
         alt="Gradient"
-        width={data.lang === "he" ? gradient.width : gradientOtherSide.width}
-        height={data.lang === "he" ? gradient.height : gradientOtherSide.height}
-        className={`
-          absolute top-0 
-          ${data.lang === "he" ? "left-0" : "right-0"} 
-          object-cover lg:object-containt w-max h-screen z-[-1]
-        `}
+        width={gradientOtherSide.width}
+        height={gradientOtherSide.height}
+        className="absolute top-0 ltr:right-0 rtl:left-0 object-cover lg:object-containt w-max h-screen z-[-1] rtl:scale-x-[-1]"
       />
 
       <Image
-        src={data.lang === "he" ? aboutBackgroundOtherSide : aboutBackground}
+        src={aboutBackground}
         alt="Background"
-        width={data.lang === "he" ? aboutBackgroundOtherSide.width : aboutBackground.width}
-        height={data.lang === "he" ? aboutBackgroundOtherSide.height : aboutBackground.height}
-        className="absolute top-0 left-0 object-cover w-screen h-screen z-[-2]"
+        width={aboutBackground.width}
+        height={aboutBackground.height}
+        className="absolute top-0 left-0 object-cover w-screen h-screen z-[-2] rtl:scale-x-[-1]"
       />
     </>
   )
