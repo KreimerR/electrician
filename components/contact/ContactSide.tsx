118fdc2eeac7fbc8da05912cc4d0cdfd33db835c
Rodion Kreimer <kreimerrodion@gmail.com>
2026-05-15 10:51:46 +0300

    update: update the ContactSide component

--- components/contact/ContactSide.tsx
+++ components/contact/ContactSide.tsx
@@ -7,26 +7,18 @@ export default function ContactSide() {
   const data = useDataContext()
 
   return (
-    <div className="flex flex-col items-center gap-7 lg:pt-20 w-[800px]">
+    <div className="flex flex-col items-center gap-7 lg:pt-20 w-[800px] text-brand-white">
       <h1
-        className={`text-4xl text-brand-white font-bold w-full ${data.lang === "he" ? "text-right" : "text-left"}`}
+        className="text-4xl font-bold w-full"
       >
         {data.content.contact.h1}
       </h1>
 
       <Form />
 
-      <div className="w-full">
-        <h3
-          className={`w-full text-3xl text-brand-white font-[600] ${data.lang === "he" && "text-right"}`}
-        >
-          {data.content.contact.callTitle}
-        </h3>
-        <h3
-          className={`w-full text-3xl text-brand-white font-[600] ${data.lang === "he" && "text-right"}`}
-        >
-          {data.content.contact.callPhone}
-        </h3>
+      <div className="w-full text-3xl font-[600]">
+        <h3>{data.content.contact.callTitle}</h3>
+        <h3><bdi dir="ltr">{data.content.contact.callPhone}</bdi></h3>
       </div>
     </div>
   )
